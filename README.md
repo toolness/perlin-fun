71c23633bcb3dacfc2910bbf67550a3217668e0a
Atul Varma <varmaa@gmail.com>
2013-06-09 10:44:34 -0400

    Added README.md, comment header.

---
+++ README.md
@@ -0,0 +1,14 @@
+## Perlin Noise Fun
+
+By Atul Varma, June 9 2013
+
+This is just some experimentation I did with Perlin noise.
+
+I learned the basics of the algorithm from a [Perlin noise page][1] on
+the internet, but derived a number of formulas on my own to ensure that
+I grasped the concepts. However, this means my implementation could be
+incorrect or inefficient.
+
+License is public domain.
+
+  [1]: http://freespace.virgin.net/hugo.elias/models/m_perlin.htm
--- perlin.js
+++ perlin.js
@@ -1,3 +1,15 @@
+// perlin.js
+// By Atul Varma, June 9 2013
+//
+// I learned the basics of the algorithm from a [Perlin noise page][1] on
+// the internet, but derived a number of formulas on my own to ensure that
+// I grasped the concepts. However, this means my implementation could be
+// incorrect or inefficient.
+//
+// License is public domain.
+//
+//   [1]: http://freespace.virgin.net/hugo.elias/models/m_perlin.htm
+
 var Perlin = (function(MersenneTwister) {
   var Perlin = {};
 
